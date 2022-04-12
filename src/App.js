b5970692bcf0eec84d0c85e5fb0c620900d844cb
MohamedAmineNadim <mimidamine2nadim@gmail.com>
2022-04-12 13:10:15 +0200

    Mods

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import User from './components/User';
 import Skills from './components/Skills';
 import Profile from './components/Profile';
 
+
 function App() {
+
   return (
     <div className="App">
-
       <div className="grid__container">
         <div className="sidebar">
+          <div className="actions">
+            <a href="./files/CV NADIM Mohamed Amine.pdf" download="CV NADIM Mohamed Amine.pdf">Print</a>
+          </div>
           <User/>
           <Skills/>
         </div>
